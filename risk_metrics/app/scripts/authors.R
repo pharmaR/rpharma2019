1c8d863eb2dc79fe5a6635cbaf569f28e23a0af5
Andy Nicholls <andy.p.nicholls@gsk.com>
2019-08-19 23:02:42 +0100

    Collect author metrics

---
+++ risk_metrics/app/scripts/authors.R
@@ -0,0 +1,69 @@
+# Package authors
+library(stringr)
+library(dplyr)
+library(purrr)
+library(foreach)
+# download.file("http://cran.R-project.org/web/packages/packages.rds",
+              # "packages.rds", mode="wb")
+x <- readRDS("data/packages.rds")
+
+packages <- c("haven", "dplyr", "broom", "lme4", "RBesT", "foreach")
+
+authors <- x[, c("Package", "Author")] %>%
+  as_tibble()
+
+
+authors_tidy <- authors %>%
+  pull(Author) %>%
+  str_replace_all("\n", "") %>%
+  str_replace_all("\\[aut, cre\\]", "") %>%
+  str_replace_all("\\[cre, aut\\]", "") %>%
+  str_replace_all("\\[aut, cph\\]", "") %>%
+  str_replace_all("\\[cph, fnd\\]", "") %>%
+  str_replace_all("\\[aut\\]", "") %>%
+  str_replace_all("\\[cre\\]", "") %>%
+  str_replace_all("\\[ctb\\]", "") %>%
+  str_replace_all("\\[cph\\]", "") %>%
+  str_replace_all("and ", "") %>%
+  str_split(", ") %>%
+  map(str_trim) %>%
+  unlist
+
+interest_authors <- authors %>%
+  filter(Package %in% packages) %>%
+  pull(Author) %>%
+  str_replace_all("\n", "") %>%
+  str_replace_all("\\[aut, cre\\]", "") %>%
+  str_replace_all("\\[cre, aut\\]", "") %>%
+  str_replace_all("\\[aut, cph\\]", "") %>%
+  str_replace_all("\\[cph, fnd\\]", "") %>%
+  str_replace_all("\\[aut\\]", "") %>%
+  str_replace_all("\\[cre\\]", "") %>%
+  str_replace_all("\\[ctb\\]", "") %>%
+  str_replace_all("\\[cph\\]", "") %>%
+  str_replace_all("and ", "") %>%
+  str_split(", ") %>%
+  map(str_trim) 
+
+# Remove additional bracketed info for authors
+short_list <- interest_authors %>% 
+  map(~ str_split_fixed(., "\\(", 2)) %>%
+# and any file paths
+  map(~ .[!str_detect(., "/")] ) %>%
+  map(~ .[. != ""]) %>%
+  map(str_trim) %>%
+  map(str_replace, "\\)", "")
+
+count_string <- function(x){
+  sum(str_count(authors_tidy, x))
+}
+author_counts <- foreach(i = short_list) %do% {
+  sapply(i, count_string)
+}
+
+sapply(short_list[[1]], count_string) %>% sum
+sapply(short_list[[2]], count_string) %>% sum
+sapply(short_list[[3]], count_string) %>% sum
+sapply(short_list[[4]], count_string) %>% sum
+sapply(short_list[[5]], count_string) %>% sum
+sapply(short_list[[6]], count_string) %>% sum
